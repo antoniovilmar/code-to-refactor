b63e7799bc9651906e55bdce70cb17b1fabe0a75
Antonio Castro <antonios@db.tec.br>
2022-09-14 17:36:42 -0300

    Ajustando domain

--- src/main/java/com/example/demo/domain/conta/ContaCorrente.java
+++ src/main/java/com/example/demo/domain/conta/ContaCorrente.java
@@ -1,12 +1,8 @@
 package com.example.demo.domain.conta;
 
-import com.example.demo.domain.Dependente;
 import com.example.demo.domain.TipoConta;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 public class ContaCorrente extends Conta {
@@ -14,21 +10,14 @@ public class ContaCorrente extends Conta {
     private ContaCorrente() {
         super();
     }
-    @OneToMany
-    private Set<Dependente> dependentes;
 
     public ContaCorrente(long number, long agency, String holderCPF) {
         super(number, agency, holderCPF, TipoConta.CORRENTE);
-        dependentes = new HashSet<>();
     }
 
     @Override
     protected double getSaldoMinimoPermitido() {
         return -1000;
-    }
-
-    public void incluirDependente(final Dependente dependente) {
-        this.dependentes.add(dependente);
     }
 
 }
